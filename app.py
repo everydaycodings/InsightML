6487d2b1482ae67f1a29864a68376b88c9838cf6
Kumar Saksham <everydaycodings@gmail.com>
2023-04-16 22:15:05 +0530

    added logistic regression

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-from helper import Utils, ApplyLinearRegression
+from helper import Utils, ApplyLinearRegression, ApplyLogisticRegression
 
 
 models_list = ["Linear Regression", "Logistic Regression"]
 linear_regression_model_metrics = ["MAE", "MSE", "RMSE", "R2", "Adjusted R2", "Cross Validated R2"]
+logistic_regression_model_metrics = ["Confusion Metrics", "ROC Curve", "Precision Recall Curve"]
 
 st.sidebar.title("Welcome to InsightML")
 
@@ -17,7 +18,7 @@ if file_upload is not None:
     data = Utils().read_file(file_upload)
     
     if st.sidebar.checkbox("Display Data", value=True):
-        st.subheader("Uploaded Dataset")
+        st.title("Uploaded Dataset")
         st.dataframe(data)
     
     with st.sidebar.expander("ML General Settings"):
@@ -44,24 +45,30 @@ if file_upload is not None:
     
 
     with st.sidebar.expander("Hyperparameters"):
-        if None not in linear_model_selector:
-                apha_value_selector = st.number_input("Enter Your Alpha Value: ", min_value=0.0, value=1.0)
-                l1_ratio_selector = st.number_input("L1 ratio for ElasticNet(optional): ", min_value=0.1, max_value=1.0, value=0.5)
-    
+
+        if model_selector == "Linear Regression":
+            if None not in linear_model_selector:
+                    apha_value_selector = st.number_input("Enter Your Alpha Value: ", min_value=0.0, value=1.0)
+                    l1_ratio_selector = st.number_input("L1 ratio for ElasticNet(optional): ", min_value=0.1, max_value=1.0, value=0.5)
+
+        if model_selector == "Logistic Regression":
+            penalty_selector = st.selectbox("Select the penalty: ", options=["l1", "l2", "elasticnet", None], index=1)
+
 
     with st.sidebar.expander("Choose Metrics To show"):
 
         if model_selector == "Linear Regression":
             metrics_selector = st.multiselect("Select the metrics to be ploted: ", options=linear_regression_model_metrics, default=linear_regression_model_metrics)
 
-
+        elif model_selector == "Logistic Regression":
+            metrics_selector = st.multiselect("Select the metrics to be ploted: ", options=logistic_regression_model_metrics, default=logistic_regression_model_metrics)
 
     if st.sidebar.button("Evaluate"):
 
         if model_selector == "Linear Regression":
 
             linear_metrics_dict = ApplyLinearRegression(X_train, X_test, y_train, y_test).apply_model(metrics_selector)
-            st.subheader("Metrics Result: ")
+            st.title("{} Result: ".format(model_selector))
             
             if None not in linear_model_selector:
 
@@ -92,3 +99,17 @@ if file_upload is not None:
                 for key, value in linear_metrics_dict.items():
                     if value != None:
                         st.text("{}: {}".format(key, value))
+        
+
+        elif model_selector == "Logistic Regression":
+
+            accuracy, recall, precision, f1, auc, y_pred, regression_ = ApplyLogisticRegression(X_train, X_test, y_train, y_test).apply_model(penalty_selector)
+            st.title("{} Result: ".format(model_selector))
+            st.write("Accuracy: ", accuracy)
+            st.write("Recall: ", recall)
+            st.write("Precision: ", precision)
+            st.write("F1 Score: ", f1)
+            st.write("AUC Score: ", auc)
+            
+            st.title("Plot Metrics")
+            ApplyLogisticRegression(X_train, X_test, y_train, y_test).plot_metrics(y_pred,regression_, metrics_selector)
--- helper.py
+++ helper.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import cross_val_score
 import numpy as np
 from sklearn import metrics
 from sklearn.linear_model import Ridge, ElasticNet, Lasso
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, ConfusionMatrixDisplay, confusion_matrix, RocCurveDisplay, PrecisionRecallDisplay, precision_recall_curve
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tempfile
+
 
 class Utils:
 
@@ -137,3 +142,70 @@ class ApplyLinearRegression:
         metrics = self.Reg_Models_Evaluation_Metrics(regression,self.X_train,self.y_train,self.X_test,self.y_test,y_pred, metrics_selector)
 
         return metrics
+
+
+class ApplyLogisticRegression:
+
+    def __init__(self, X_train, X_test, y_train, y_test):
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+    
+
+    def plot_metrics(self, y_pred, clf, metrics_selector):
+        
+        if "Confusion Metrics" in metrics_selector:
+            st.subheader("Confusion Metrics")
+            cm = confusion_matrix(self.y_test, y_pred, labels=clf.classes_)
+            st.text(cm)
+            plt.figure(figsize=(10, 7))
+            sns.heatmap(cm, annot=True)
+            plt.xlabel("Predicted")
+            plt.ylabel("Truth")
+
+            with tempfile.TemporaryDirectory() as path:
+                img_path = '{}/my_plot.png'.format(path)
+                plt.savefig(img_path)
+                st.image(img_path)
+        
+
+        if "ROC Curve" in metrics_selector:
+            st.subheader("ROC Curve")
+            roc_auc = roc_auc_score(self.y_test, y_pred)
+            roc_display = RocCurveDisplay.from_predictions(self.y_test, y_pred)
+            roc_display.ax_.set_title('ROC Curve (AUC = {:0.2f})'.format(roc_auc))
+            with tempfile.TemporaryDirectory() as path:
+                img_path = '{}/my_plot.png'.format(path)
+                plt.savefig(img_path)
+                st.image(img_path)
+        
+        if "Precision Recall Curve" in metrics_selector:
+            st.subheader("Precision Recall Curve")
+            plt.figure(figsize=(10, 7))
+            precision, recall, thresholds = precision_recall_curve(self.y_test, y_pred)
+            plt.plot(recall, precision)
+            plt.xlabel('Recall')
+            plt.ylabel('Precision')
+            plt.title('Precision-Recall Curve')
+            with tempfile.TemporaryDirectory() as path:
+                img_path = '{}/my_plot.png'.format(path)
+                plt.savefig(img_path)
+                st.image(img_path)
+
+
+
+
+    def apply_model(self, penalty):
+
+        regression = LogisticRegression(penalty=penalty)
+        regression.fit(self.X_train,self.y_train)
+        y_pred = regression.predict(self.X_test)
+        accuracy = round(accuracy_score(self.y_test, y_pred), 3)
+        recall = round(recall_score(self.y_test, y_pred), 3)
+        precision = round(precision_score(self.y_test, y_pred), 3)
+        f1 = round(f1_score(self.y_test, y_pred), 3)
+        auc = round(roc_auc_score(self.y_test, y_pred), 3)
+
+        return accuracy, recall, precision, f1, auc, y_pred, regression
